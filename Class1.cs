fe93f85cf5feb7b123c8ee59ed0da56a187ad6c4
Syoshi7 <155185064+Syoshi7@users.noreply.github.com>
2024-11-22 09:32:54 +0400

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Lab_4
+{
+    internal class Tasks
+    {
+        public static int IntEnter()                         // Метод для проверки числа на правильность ввода.
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите целое число: ");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out int number))
+                    return number;
+                else
+                    Console.WriteLine("Ошибка ввода.");
+            }
+        }
+
+
+        public static void deleteElementFromList(List<int> list, int E)
+        {
+            int HowManyE = 0;
+            foreach (int i in list)
+            {
+                if (i == E)
+                    HowManyE++;
+            }
+
+            if (HowManyE == 0)
+                Console.WriteLine("Элемента E нет в нашем списке.");
+            else
+            {
+                for (int i = 0; i < HowManyE; i++)
+                    list.Remove(E);
+            }
+        }
+
+
+        public static void reverseLinkedList(LinkedList<int> list)
+        {
+            var currentNode = list.Last;
+            while (currentNode != null)
+            {
+                Console.WriteLine(currentNode.Value);
+                currentNode = currentNode.Previous;
+            }
+        }
+
+        public static void analysisForTask3(List<HashSet<string>> data, HashSet<string> firm)
+        {
+            var all = new HashSet<string>(firm);
+            var atleastone = new HashSet<string>();
+            var none = new HashSet<string>(firm);
+
+            foreach (var shop in data)
+            {
+                all.IntersectWith(shop); // возвращает общие элементы
+                atleastone.UnionWith(shop); // возвращает все элементы, объединяет два сета
+                none.ExceptWith(shop); // возвращает элементы, которые есть в первом сете, но нет во втором
+            }
+
+            Console.WriteLine("Фирмы, у которых все закупали: ");
+            foreach (var firms in all)
+            {
+                Console.WriteLine(firms);
+            }
+
+            Console.WriteLine("\nФирмы, у которых брало хотя бы 1 заведение: ");
+            foreach (var firms in atleastone)
+            {
+                Console.WriteLine(firms);
+            }
+
+            Console.WriteLine("\nФирмы, у которых не заказывали пк: ");
+            foreach (var firms in none)
+            {
+                Console.WriteLine(firms);
+            }
+        }
+
+        public static void SonoursConsonants(string filePath)
+        {
+            HashSet<char> SonoursConsonants = ['б', 'в', 'г', 'д', 'ж', 'з', 'л', 'м', 'н', 'р'];
+
+            HashSet<char> foundConsonants = new HashSet<char>();
+
+            string text = File.ReadAllText(filePath);
+            Console.WriteLine("Содержимое файла:");
+            Console.WriteLine(text);
+
+            foreach (var word in text.Split(new char[] { ' ', '\n', '.', ',', '!', '?', ';', ':', '-' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                foreach (char c in word.ToLower())
+                {
+                    if (SonoursConsonants.Contains(c))
+                        foundConsonants.Add(c);
+
+                }
+            }
+
+            var sortedConsonants = foundConsonants.OrderBy(c => c).ToList();
+
+            if (sortedConsonants.Count > 0)
+            {
+                Console.WriteLine("Звонкие согласные в алфавитном порядке:");
+                foreach (var consonant in sortedConsonants)
+                {
+                    Console.Write(consonant + " ");
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Звонкие согласные не найдены.");
+            }
+        }
+
+
+        private static void FifthTaskFileCreate(string filePath5, List<(string LastName, string FirstName)> students)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<(string LastName, string FirstName)>));
+            using (StreamWriter writer = new StreamWriter(filePath5, false, Encoding.UTF8))
+                serializer.Serialize(writer, students);
+        }
+
+        private static List<(string LastName, string FirstName)> FifthTaskReadFile(string filePath5)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<(string LastName, string FirstName)>));
+            using (StreamReader reader = new StreamReader(filePath5))
+            {
+                return (List<(string LastName, string FirstName)>)serializer.Deserialize(reader) ?? throw new InvalidOperationException("Файл пуст");
+            }
+        }
+        private static void ValidData(string lastName, string firstName)
+        {
+            if (lastName.Length == 0 || firstName.Length == 0)
+            {
+                throw new Exception("Фамилия и имя не могут быть пустыми.");
+            }
+
+            if (lastName.Length > 20)
+            {
+                throw new Exception("Фамилия не может быть длиннее 20 символов.");
+            }
+
+            if (firstName.Length > 15)
+            {
+                throw new Exception("Имя не может быть длиннее 15 символов.");
+            }
+        }
+
+        public static void studentInput(string filePath5)
+        {
+            Console.WriteLine("Введите количество абитуриентов: ");
+            int count = IntEnter();
+            while (true)
+            {
+                if (count < 1)
+                    Console.WriteLine("Количество студентов не может быть меньше 1");
+                if (count > 100)
+                    Console.WriteLine("Количество студентов не может быть больше 100");
+                else
+                    break;
+            }
+
+            var students = new List<(string LastName, string FirstName)>();
+            Console.WriteLine("Введите данные абитуриентов в формате: Фамилия Имя");
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write("Введите данные студента " + (i + 1) + ": ");
+                string[] input = Console.ReadLine().Split(' ');
+                string lastName = input[0];
+                string firstName = input[1];
+
+                ValidData(lastName, firstName);
+
+                students.Add((lastName, firstName));
+            }
+            FifthTaskFileCreate(filePath5, students);
+        }
+
+        private static List<string> GenerateLogins(List<(string LastName, string FirstName)> students)
+        {
+            Dictionary<string, int> loginCounter = new Dictionary<string, int>();
+            List<string> logins = new List<string>();
+
+            foreach (var student in students)
+            {
+                string baseLogin = $"{student.LastName}";
+                if (!loginCounter.ContainsKey(baseLogin))
+                {
+                    loginCounter[baseLogin] = 1;
+                    logins.Add(baseLogin);
+                }
+                else
+                {
+                    loginCounter[baseLogin]++;
+                    logins.Add($"{baseLogin}{loginCounter[baseLogin]}");
+                }
+            }
+            return logins;
+        }
+
+        public static void FifthTask(string filePath5)
+        {
+            var students = FifthTaskReadFile(filePath5);
+            List<string> logins = GenerateLogins(students);
+
+            Console.WriteLine("Сгенерированные логины:");
+            for (int i = 0; i < students.Count; i++)
+            {
+                Console.WriteLine(logins[i]);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using System.Text;
+namespace Lab_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            //===================================================
+            // Задание 1
+            //===================================================
+
+            Console.WriteLine("Лабораторная работа 4, В-1");
+            Console.WriteLine("\n=======================================================================\n");
+            Console.WriteLine("Задание 1. Составить программу, которая удаляет из списка L все элементы E, если такие есть.");
+            Console.WriteLine("Требуется задать длину списка.");
+
+            int LenghtList;
+            while (true)
+            {
+                int LenghtCheck = Tasks.IntEnter();                // Указываем длину листа
+                if (LenghtCheck > 0)
+                {
+                    LenghtList = LenghtCheck;
+                    break;
+                }
+                else
+                    Console.WriteLine("Длина списка не может быть отрицательной, а также в нашем случае список не должен быть пустой");
+            }
+            List<int> list = new List<int>(LenghtList);
+
+            for (int i = 0; i < LenghtList; i++)        // Заполнение листа
+            {
+                Console.WriteLine("Введите значение элемента номер " + (i + 1) + ": ");
+                int number = Tasks.IntEnter();
+                list.Add(number);
+            }
+
+            Console.Write("[");                 // Показываем готовый списочек
+            foreach (int i in list)
+                Console.Write(i + " ");
+            Console.Write("]");
+
+
+            Console.WriteLine("\nВведите элемент E, который должен быть удалён из списка.");
+            int E = Tasks.IntEnter();
+
+            Tasks.deleteElementFromList(list, E);
+
+            Console.Write("[");                 // Показываем изменённый списочек
+            foreach (int i in list)
+            {
+                Console.Write(i + " ");
+            }
+            Console.Write("]");
+
+            //=====================================================================
+            // Задание 2.
+            //=====================================================================
+
+            Console.WriteLine("\n\n=======================================================================");
+            Console.WriteLine("\nЗадание 2. Напечатать в обратном порядке элементы непустого списка L.");
+
+            LinkedList<int> LinList = new LinkedList<int>([1, 2, 3, 4, 5, 10, 9, 8, 7, 6]);
+
+            Tasks.reverseLinkedList(LinList);
+
+            //=====================================================================
+            // Задание 3.
+            //=====================================================================
+
+            Console.WriteLine("\n\n=======================================================================");
+            Console.WriteLine("\nЗадание 3.");
+
+            List<HashSet<string>> buyingData =
+            [
+                new HashSet<string> {"ДНС", "Озончик", "Вайлдберриз"},
+                new HashSet<string> {"ДНС", "НашПКМагазин", "Всё для дома"},
+                new HashSet<string> {"ДНС", "Озончик", "Всё для дома"}
+            ];
+
+            HashSet<string> firm = ["ДНС", "Озончик", "Вайлдберриз", "НашПКМагазин", "Всё для дома", "ПермьПК"];
+
+            Tasks.analysisForTask3(buyingData, firm);
+
+            //=====================================================================
+            // Задание 4.
+            //=====================================================================
+
+            Console.WriteLine("\n\n=======================================================================");
+            Console.WriteLine("\nЗадание 4.");
+
+            string filePath = "C:\\Users\\Syoshi\\source\\repos\\Lab 4\\text.txt";
+            Tasks.SonoursConsonants(filePath);
+
+            //=====================================================================
+            // Задание 5.
+            //=====================================================================
+
+            Console.WriteLine("\n\n=======================================================================");
+            Console.WriteLine("\nЗадание 5.");
+
+            string filePath5 = "C:\\Users\\Syoshi\\source\\repos\\Lab 4\\task5.xml";
+            Tasks.studentInput(filePath5);
+            Tasks.FifthTask(filePath5);
+        }
+    }
+}
